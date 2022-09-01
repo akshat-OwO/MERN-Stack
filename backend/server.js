fe132122a517acf604c77c85d4b71889fbec784a
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-09-01 14:22:23 +0530

    Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const workoutRoutes = require('./routes/workouts.js');
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
@@ -15,8 +17,9 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'});
 });
+app.use('/api/workouts', workoutRoutes);
 
 // listen for requests
 app.listen(process.env.PORT, () => {
     console.log('listening on port', process.env.PORT);
-}); 
+});
