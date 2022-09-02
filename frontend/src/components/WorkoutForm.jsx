5b086c65a19d1df2337d826c6519598f43e3bdca
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-09-02 11:43:20 +0530

    Handling Error Responses

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
     const [ load, setLoad ] = useState('');
     const [ reps, setReps ] = useState('');
     const [ error, setError ] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -25,9 +26,11 @@ const WorkoutForm = () => {
 
         if(!response.ok) {
             setError(json.error);
+            setEmptyFields(json.emptyFields);
         }
         if(response.ok) {
             setError(null);
+            setEmptyFields([]);
             setTitle('');
             setLoad('');
             setReps('');
@@ -45,18 +48,21 @@ const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
             <label>Load (in kg):</label>
             <input 
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
             <label>Reps:</label>
             <input 
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
             <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
